b6347776b33e992d8f78dcc15cc474f09c77965f
Brandon McGuire <brandonmcguire1992@gmail.com>
2020-10-15 21:03:39 -0500

    offline functionality

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
+
 
 const [, dispatch] = useStoreContext();
 
@@ -9,6 +11,7 @@ const removeFromCart = item => {
     type: REMOVE_FROM_CART,
     _id: item._id
   });
+  idbPromise('cart', 'delete', { ...item });
 };
 
 
@@ -52,12 +55,16 @@ const onChange = (e) => {
       type: REMOVE_FROM_CART,
       _id: item._id
     });
+  
+    idbPromise('cart', 'delete', { ...item });
   } else {
     dispatch({
       type: UPDATE_CART_QUANTITY,
       _id: item._id,
       purchaseQuantity: parseInt(value)
     });
+  
+    idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
   }
 };
 
